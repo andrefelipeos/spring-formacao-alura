3b60afdf534041f809ca54a05c1bf6e974c11903
André Sousa <andre.lipe.o@gmail.com>
2022-10-26 17:03:13 -0300

    feat: adds runtime exception 'DAOException'

---
+++ curso-iv-jdbc/src/main/java/alura/spring/jdbc/repository/DAOException.java
@@ -0,0 +1,22 @@
+package alura.spring.jdbc.repository;
+
+public class DAOException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DAOException() {
+		super();
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+}
