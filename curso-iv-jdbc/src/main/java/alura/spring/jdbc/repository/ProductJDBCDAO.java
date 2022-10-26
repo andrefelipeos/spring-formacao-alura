bd0fa9a0919ba904177203c3ddcc00ee9f231ef4
André Sousa <andre.lipe.o@gmail.com>
2022-10-26 13:32:54 -0300

    feat: adds class 'ProductJDBCDAO' that implements 'ProductDAO'

---
+++ curso-iv-jdbc/src/main/java/alura/spring/jdbc/repository/ProductJDBCDAO.java
@@ -0,0 +1,21 @@
+package alura.spring.jdbc.repository;
+
+import java.util.List;
+
+import alura.spring.jdbc.models.Product;
+
+public class ProductJDBCDAO implements ProductDAO {
+
+	public void deleteByIdentifier(int identifier) {
+
+	}
+
+	public List<Product> findAll() {
+		return null;
+	}
+
+	public void save(Product product) {
+
+	}
+
+}
