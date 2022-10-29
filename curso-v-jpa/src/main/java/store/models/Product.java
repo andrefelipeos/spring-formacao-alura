7a1aaf140902bcd74cab6fceb86f95f0e7377edc
André Sousa <andre.lipe.o@gmail.com>
2022-10-28 22:15:04 -0300

    feat: adds model 'Product' as a JPA entity

---
+++ curso-v-jpa/src/main/java/store/models/Product.java
@@ -0,0 +1,61 @@
+package store.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer identifier;
+	private String name;
+	private String description;
+	private BigDecimal price;
+
+	public Product(String name, String description, BigDecimal price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Integer getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(Integer identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+}
