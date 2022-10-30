841bad7610555bec64a38fceae3a11d5809d53dc
André Sousa <andre.lipe.o@gmail.com>
2022-10-30 13:12:50 -0300

    temp: adds class Main for testing

---
+++ curso-v-jpa/src/main/java/store/Main.java
@@ -0,0 +1,20 @@
+package store;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import store.models.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product mobile = new Product("XPhone", "Octacore, 2GB", new BigDecimal(1200));
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgresql");
+		EntityManager em = emf.createEntityManager();
+		em.persist(mobile);
+	}
+
+}
