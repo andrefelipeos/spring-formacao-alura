f0218b36fa42e6d98ffe0a5fe4b1414f48efa91e
André Sousa <andre.lipe.o@gmail.com>
2022-10-30 20:29:15 -0300

    feat: creates class JPAUtil as a EntityManager factory

---
+++ curso-v-jpa/src/main/java/store/util/JPAUtil.java
@@ -0,0 +1,15 @@
+package store.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("postgresql");
+
+	public static EntityManager getEntityManager() {
+		return FACTORY.createEntityManager();
+	}
+
+}
