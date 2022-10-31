6d679cde582d82fb615c5be67fb51896799a5e6b
André Sousa <andre.lipe.o@gmail.com>
2022-10-31 10:25:53 -0300

    temp: inserts category and product into database

--- curso-v-jpa/src/main/java/store/Main.java
+++ curso-v-jpa/src/main/java/store/Main.java
@@ -3,20 +3,26 @@ package store;
 import java.math.BigDecimal;
 
 import jakarta.persistence.EntityManager;
+import store.dao.CategoryDao;
+import store.dao.CategoryDaoImpl;
 import store.dao.ProductDao;
 import store.dao.ProductDaoImpl;
+import store.models.Category;
 import store.models.Product;
 import store.util.JPAUtil;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Product mobile = new Product("XPhone", "Octacore, 2GB", new BigDecimal(1200));
+		Category mobiles = new Category("MOBILES");
+		Product mobile = new Product("XPhone", "Octacore, 2GB", new BigDecimal(1200), mobiles);
 
 		EntityManager em = JPAUtil.getEntityManager();
+		CategoryDao categoryDao = new CategoryDaoImpl(em);
 		ProductDao productDao = new ProductDaoImpl(em);
 
 		em.getTransaction().begin();
+		categoryDao.save(mobiles);
 		productDao.save(mobile);
 		em.getTransaction().commit();
 		em.close();
