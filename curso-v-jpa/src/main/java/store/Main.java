ef2c0ae2879cc9378b5be945db2905adf1b657a9
André Sousa <andre.lipe.o@gmail.com>
2022-11-03 17:36:10 -0300

    temp: updates 'Main' tests

--- curso-v-jpa/src/main/java/store/Main.java
+++ curso-v-jpa/src/main/java/store/Main.java
@@ -1,31 +1,15 @@
 package store;
 
-import java.math.BigDecimal;
-
-import jakarta.persistence.EntityManager;
-import store.dao.CategoryDao;
 import store.dao.CategoryDaoImpl;
-import store.dao.ProductDao;
-import store.dao.ProductDaoImpl;
 import store.models.Category;
-import store.models.Product;
-import store.util.JPAUtil;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Category mobiles = new Category("MOBILES");
-		Product mobile = new Product("XPhone", "Octacore, 2GB", new BigDecimal(1200), mobiles);
-
-		EntityManager em = JPAUtil.getEntityManager();
-		CategoryDao categoryDao = new CategoryDaoImpl(em);
-		ProductDao productDao = new ProductDaoImpl(em);
-
-		em.getTransaction().begin();
-		categoryDao.save(mobiles);
-		productDao.save(mobile);
-		em.getTransaction().commit();
-		em.close();
+		CategoryDaoImpl categoryDao = new CategoryDaoImpl();
+		Category toChange = categoryDao.findByIdentifier(13);
+		toChange.setName("CLOTHES");
+		categoryDao.save(toChange);
 	}
 
 }
