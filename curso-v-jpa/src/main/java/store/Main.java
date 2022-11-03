68d0d95780d0f48c967a7fae63cd19c716961720
André Sousa <andre.lipe.o@gmail.com>
2022-11-03 17:55:51 -0300

    temp: updates 'Main' tests

--- curso-v-jpa/src/main/java/store/Main.java
+++ curso-v-jpa/src/main/java/store/Main.java
@@ -1,15 +1,26 @@
 package store;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import store.dao.CategoryDaoImpl;
+import store.dao.ProductDao;
+import store.dao.ProductDaoImpl;
 import store.models.Category;
+import store.models.Product;
 
 public class Main {
 
 	public static void main(String[] args) {
-		CategoryDaoImpl categoryDao = new CategoryDaoImpl();
-		Category toChange = categoryDao.findByIdentifier(13);
-		toChange.setName("CLOTHES");
-		categoryDao.save(toChange);
+		Category clothes = new CategoryDaoImpl().findByIdentifier(13);
+		Product tshirt = new Product("Basic 0", "White basic T-shirt", new BigDecimal(40), clothes);
+		ProductDao pd = new ProductDaoImpl();
+		pd.save(tshirt);
+
+		List<Product> listOfClothes = pd.findByCategory(clothes);
+		for (Product product : listOfClothes) {
+			System.out.println(product.getName() + ": " + product.getDescription());
+		}
 	}
 
 }
