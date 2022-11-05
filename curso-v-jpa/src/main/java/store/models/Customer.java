a35a48931e7ff7dfd5321709b9b784eaa2d380f4
André Sousa <andre.lipe.o@gmail.com>
2022-11-04 21:30:04 -0300

    feat: creates new entity class 'Customer'

---
+++ curso-v-jpa/src/main/java/store/models/Customer.java
@@ -0,0 +1,51 @@
+package store.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer identifier;
+	private String name;
+	private String cpf;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String nome, String cpf) {
+		super();
+		this.name = nome;
+		this.cpf = cpf;
+	}
+
+	public Integer getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(Integer identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
