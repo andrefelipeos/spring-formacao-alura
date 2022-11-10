d1ecf093f2017d57b4cf399b0d7b6b72ab271931
André Sousa <andre.lipe.o@gmail.com>
2022-11-10 17:16:22 -0300

    temp: updates main tests

--- curso-v-jpa/src/main/java/store/Main.java
+++ curso-v-jpa/src/main/java/store/Main.java
@@ -1,8 +1,8 @@
 package store;
 
 import java.math.BigDecimal;
-import java.util.List;
 
+import store.dao.CategoryDao;
 import store.dao.CategoryDaoImpl;
 import store.dao.ProductDao;
 import store.dao.ProductDaoImpl;
@@ -12,15 +12,24 @@ import store.models.Product;
 public class Main {
 
 	public static void main(String[] args) {
-		Category clothes = new CategoryDaoImpl().findByIdentifier(13);
-		Product tshirt = new Product("Basic 0", "White basic T-shirt", new BigDecimal(40), clothes);
-		ProductDao pd = new ProductDaoImpl();
-		pd.save(tshirt);
+		CategoryDao categoryDao = new CategoryDaoImpl();
+		ProductDao productDao = new ProductDaoImpl();
 
-		List<Product> listOfClothes = pd.findByCategory(clothes);
-		for (Product product : listOfClothes) {
-			System.out.println(product.getName() + ": " + product.getDescription());
-		}
+		Category book = new Category("BOOK");
+		Category cloth = new Category("CLOTH");
+
+		book = categoryDao.save(book);
+		cloth = categoryDao.save(cloth);
+
+		Product book01 = new Product("Daring Greatly", "by Brené Brown", new BigDecimal(20), book);
+		Product book02 = new Product("The ABC crimes", "by Agatha Christie", new BigDecimal(20), book);
+		Product tshirt01 = new Product("T-shirt I", "green, with a pocket", new BigDecimal(50), cloth);
+		Product tshirt02 = new Product("T-shirt II", "blue, with a nice symbol", new BigDecimal(50), cloth);
+
+		productDao.save(book01);
+		productDao.save(book02);
+		productDao.save(tshirt01);
+		productDao.save(tshirt02);
 	}
 
 }
