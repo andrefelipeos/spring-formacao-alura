12dc082e76403157dc5a2625523afed1bd6352db
André Sousa <andre.lipe.o@gmail.com>
2022-11-11 20:58:20 -0300

    feat: creates class 'OrderDaoImpl' that implements 'OrderDao'

---
+++ curso-v-jpa/src/main/java/store/dao/OrderDaoImpl.java
@@ -0,0 +1,42 @@
+package store.dao;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import store.models.Order;
+import store.util.JPAUtil;
+
+public class OrderDaoImpl implements OrderDao {
+
+	private EntityManager entityManager = null;
+
+	public OrderDaoImpl() {
+		this.entityManager = JPAUtil.getEntityManager();
+	}
+
+	@Override
+	public void deleteByIdentifier(int identifier) {
+		entityManager.getTransaction().begin();
+		entityManager.remove(findByIdentifier(identifier));
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public List<Order> findAll() {
+		return entityManager.createQuery("SELECT o FROM Order o", Order.class).getResultList();
+	}
+
+	@Override
+	public Order findByIdentifier(int identifier) {
+		return entityManager.find(Order.class, identifier);
+	}
+
+	@Override
+	public Order save(Order order) {
+		entityManager.getTransaction().begin();
+		order = entityManager.merge(order);
+		entityManager.getTransaction().commit();
+		return order;
+	}
+
+}
