cc8ee1bc905fec25793fb8f5a10895cc84a6e387
André Sousa <andre.lipe.o@gmail.com>
2022-11-11 22:37:45 -0300

    refactor: changes column name for 'unitPrice' in 'ItemOrder'

--- curso-v-jpa/src/main/java/store/models/ItemOrder.java
+++ curso-v-jpa/src/main/java/store/models/ItemOrder.java
@@ -2,6 +2,7 @@ package store.models;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,8 @@ public class ItemOrder {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer identifier;
+
+	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
 	private int amount;
 
