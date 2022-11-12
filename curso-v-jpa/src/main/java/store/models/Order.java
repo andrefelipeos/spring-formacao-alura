001722ff04a8240521f78857f51fb1fc79c279e7
André Sousa <andre.lipe.o@gmail.com>
2022-11-11 22:36:59 -0300

    refactor: changes column name for 'totalValue' in 'Order'

--- curso-v-jpa/src/main/java/store/models/Order.java
+++ curso-v-jpa/src/main/java/store/models/Order.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer identifier;
+
+	@Column(name = "total_value")
 	private BigDecimal totalValue;
 	private LocalDate date = LocalDate.now();
 
